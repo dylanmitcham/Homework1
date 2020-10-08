d0ef13398d45366fa278d06c538e20440974a18d
dylan <dylan@DESKTOP-SMHG7CG>
2020-10-07 18:58:01 -0700

    Methods were defined and the documentation was updated.
    Also, a private string variable was added to store all of the user
    inputs in order which is then returned in the toString method so we can
    see how the final total value was calculated i.e "0 + 3 - 6".

--- CS360_Assignment1/src/cse360assignment02/AddingMachine.java
+++ CS360_Assignment1/src/cse360assignment02/AddingMachine.java
@@ -2,32 +2,39 @@ package cse360assignment02; // package
 
 public class AddingMachine 
 {
-  private int total;
+  private int total; // stores current total integer value 
+  private String result; // string variable used to store history of user input for use in the toString method
   
   public AddingMachine () // default constructor class
   {
+	result = "0";
     total = 0;   // not needed - included for clarity
   }
   
   public int getTotal () // method that returns the total
   {
-    return 0; // this method wouldn't work because it returns the integer 0 instead of the integer stored in the variable named total
+    return total; // simply return total variable
   }
   
-  public void add (int value) // currently empty method that takes in an integer and adds it to the total of the AddingMachine class object
+  public void add (int value) // method that takes in an integer and adds it to the total value of the AddingMachine class object
   {
+	  result = result + " + " + value;
+	  total = total + value;
   }
 
-  public void subtract (int value) // currently empty method that takes in an integer and subtracts it from the total variable of the AddingMachine class object
+  public void subtract (int value) // method that takes in an integer and subtracts it from the total variable of the AddingMachine class object
   {
+	  result = result + " - " + value;
+	  total = total - value;
   }
 
-  public String toString () // method that returns the value of the total variable in the class object as a string 
+  public String toString () // method that returns a string of every input and the operations between them i.e "0 - 2 + 1" would be printed if the user subracted 2 and then added 1 to the default total of 0
   {
-    return ""; // this would simply return an empty string, not the total of the object represented as a string
+    return result; // result is a string which is initialized as "0" but is incremented each time the add or subtract method is used so that it shows the user's input in chronological order 
   }
-
-  public void clear() // empty method that should set the total back to 0 
+  
+  public void clear() // reverts total value back to 0
   {
+	  total = 0;
   }
 }
